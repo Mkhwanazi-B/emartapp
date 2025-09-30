ca6c94310dbeeeea8bf48eb56c965e3880b63e2e
devops4sure <blessing67mkhwanazi@gmail.com>
2025-09-30 15:33:23 +0200

    Fix database connections to use container names instead of localhost

--- nodeapi/config/keys.js
+++ nodeapi/config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
  // mongoURI: "<INSERT DATABASE URI>",
   //  mongoURI: "mongodb://emongo:27017/epoc",
-  mongoURI: process.env.MONGODB_URI || "mongodb://localhost:27017/epoc",
+  mongoURI: process.env.MONGODB_URI || "mongodb://emart-nodeapi-emongo:27017/epoc",
   secretOrKey: "k1234e6s78h9av"
 };
